10f06ba01e03e6258acd15ad122d72a6e758cefe
Jack Chou <jcacct@protonmail.ch>
2020-04-18 15:11:05 -0700

    Added interactivity to elements that should appear interactable, added appropriate styling

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,16 +3,16 @@ import { NavLink } from 'react-router-dom';
 
 export default function Header(props) {
   return (
-    <div className="container-lg p-3 px-5 text-white bg-dark">
+    <div className="container- p-3 px-5 text-white bg-dark">
       <NavLink to="/">
-        <span className="text-white">Murican Dream</span>
+        <span className="text-white interactable-element">Murican Dream</span>
       </NavLink>
       <NavLink to="/cartsummary">
         <span className="text-white float-right">
           {props.cartItemCount > 1
             ? props.cartItemCount + ' items '
             : props.cartItemCount + ' item '}
-          <i className="fas fa-shopping-cart" />
+          <i className="fas fa-shopping-cart interactable-element" />
         </span>
       </NavLink>
     </div>
