145d2b674173e0d5c49e612fea55a043afed24f8
Jack Chou <jcacct@protonmail.ch>
2020-04-19 14:31:56 -0700

    Completed initial edits of modal contents and styling

--- client/components/demo-disclaimer.jsx
+++ client/components/demo-disclaimer.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 
 export default function DemoDisclaimer(props) {
   return (
-    <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div className="modal d-block" id="demoDisclaimerModal" tabIndex="-1" role="dialog" aria-labelledby="demoDisclaimerModalTitle" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
         <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
+          <div className="modal-header d-inline">
+            <h5 className="modal-title text-center" id="demoDisclaimerModalTitle">Welcome to Murican Dream</h5>
           </div>
           <div className="modal-body">
+            Please note that this website is a content management application created for the purpose of DEMOSTRATION.
+            By choosing the accept button below you to acknowledge that the merchandise shown here is NOT available for purchase,
+            that you will NOT provide genuine financial or personal information,
+            and that you are aware no purchase will truly be processed.
           </div>
           <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" className="btn btn-primary">Save changes</button>
+            <button type="button" className="btn btn-primary"
+              onClick={() => !props.termsAccepted} data-dismiss="modal">Accept</button>
+            <button type="button" className="btn btn-secondary" data-dismiss="modal" aria-label="Close">Cancel
+            </button>
           </div>
         </div>
       </div>
