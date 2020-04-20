fd7ebd9aa00deff21a89b23ba4f19550d9c0ba32
Jack Chou <jcacct@protonmail.ch>
2020-04-20 14:58:48 -0700

    Added styling for disclaimer modal

--- client/components/demo-disclaimer.jsx
+++ client/components/demo-disclaimer.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 export default function DemoDisclaimer(props) {
   return (
-    <div className="modal d-block" id="demoDisclaimerModal" tabIndex="-1" role="dialog"
+    <div className="modal d-block bg-danger" id="demoDisclaimerModal" tabIndex="-1" role="dialog"
       aria-labelledby="demoDisclaimerModalTitle" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
-        <div className="modal-content">
+        <div className="modal-content text-primary">
           <div className="modal-header d-inline">
             <h5 className="modal-title text-center" id="demoDisclaimerModalTitle">Welcome to Murican Dream</h5>
           </div>
           <div className="modal-body">
-            Please note that this website is a content management application created for the purpose of <b>DEMOSTRATION</b>.
+            This website is a content management application created for the purpose of <b>DEMOSTRATION ONLY</b>.
             By choosing the accept button below you acknowledge that the merchandise shown here is <b>NOT</b> available for purchase,
-            that you will <b>NOT</b> provide genuine financial or personal information,
+            that you will <b>NOT</b> provide genuine personal information,
             and that you are aware no purchase will be processed.
           </div>
           <div className="modal-footer">
             <button type="button" className="btn btn-primary"
-              onClick={() => !props.termsAccepted} data-dismiss="modal">Accept</button>
+              onClick={() => props.acceptTerms()} data-dismiss="modal">Accept</button>
             <a href="https://jackmchou.com"><button type="button" className="btn btn-secondary"
-              data-dismiss="modal" aria-label="Close">Cancel</button></a>
+              data-dismiss="modal" aria-label="Close">Nah</button></a>
           </div>
         </div>
       </div>
