c4e32147e1edebb83ec982fe2de88a7daaf1d84b
Jack Chou <jcacct@protonmail.ch>
2020-04-25 07:42:52 -0700

    Edited styling for modal and fixed itemName variable

--- client/components/confirm-delete.jsx
+++ client/components/confirm-delete.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 
 export default function ConfirmDelete(props) {
+  const { cartItem } = props;
+  const itemName = cartItem.name;
+  const qty = 1;
   function deleteOperation() {
     props.showModal();
-    props.deleteCartItem(props.cartItem.cartItemId);
+    props.deleteCartItem(cartItem.cartItemId);
   }
-  const qty = 1;
-  const itemName = 'name';
   if (!props.show) return null;
   return (
     <div className="modal d-block" id="demoDisclaimerModal" tabIndex="-1" role="dialog"
       aria-labelledby="demoDisclaimerModalTitle" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
-        <div className="modal-content text-primary">
+        <div className="modal-content">
           <div className="modal-header d-inline">
-            <h5 className="modal-title text-center" id="demoDisclaimerModalTitle">Welcome to Murican Dream</h5>
+            <h5 className="modal-title text-center" id="demoDisclaimerModalTitle">Please confirm</h5>
           </div>
           <div className="modal-body">
             {`Are you sure you want to remove ${qty} of ${itemName} from your cart?`}
