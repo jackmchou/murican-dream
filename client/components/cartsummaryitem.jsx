acf42a6145e713e41e13cace257bb76c2f3eee98
Jack Chou <jcacct@protonmail.ch>
2020-04-25 07:36:49 -0700

    Edited cartsummaryitem component into a class component and implemented confirm delete modal component

--- client/components/cartsummaryitem.jsx
+++ client/components/cartsummaryitem.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ConfirmDelete from './confirm-delete';
 
-export default function CartSummaryItem(props) {
-  const cartItem = props.item;
-  return (
-    <div className="card m-3">
-      <div className="row no-gutters">
-        <div className="col-md-4">
-          <img src={cartItem.image} className="card-img" alt={cartItem.name} />
-        </div>
-        <span className="col-md-8 text-vertical">
-          <div className="card-body">
-            <h5 className="card-title">{cartItem.name}</h5>
-            <p className="card-subtitle text-muted">$ {(cartItem.price * 0.01).toFixed(2)}</p>
-            <p className="card-text">{cartItem.shortDescription}</p>
-            <button className="btn btn-danger" onClick={() => props.deleteCartItems(cartItem.cartItemId)}>Remove from Cart</button>
+export default class CartSummaryItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+    this.showModal = this.showModal.bind(this);
+  }
+
+  showModal() {
+    this.setState({ show: !this.state.show });
+  }
+
+  render() {
+    const cartItem = this.props.item;
+    return (
+      <div className="card m-3">
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img src={cartItem.image} className="card-img" alt={cartItem.name} />
           </div>
-        </span>
+          <span className="col-md-8 text-vertical">
+            <div className="card-body">
+              <h5 className="card-title">{cartItem.name}</h5>
+              <p className="card-subtitle text-muted">$ {(cartItem.price * 0.01).toFixed(2)}</p>
+              <p className="card-text">{cartItem.shortDescription}</p>
+              <button className="btn btn-danger" onClick={this.showModal}>Remove from Cart</button>
+            </div>
+          </span>
+        </div>
+        <ConfirmDelete showModal={this.showModal} show={this.state.show}
+          deleteCartItem={this.props.deleteCartItem}
+          cartItem={cartItem} />
       </div>
-    </div>
-  );
+    );
+  }
 }
