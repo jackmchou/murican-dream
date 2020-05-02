bc26d9b3ae4cf44e8f1c164e45b5e94a4128228a
Jack Chou <jcacct@protonmail.ch>
2020-05-02 06:12:25 -0700

    Edited content of modal body

--- client/components/demo-disclaimer.jsx
+++ client/components/demo-disclaimer.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 
 export default function DemoDisclaimer(props) {
   return (
-    <div className="modal d-block bg-danger" id="demoDisclaimerModal" tabIndex="-1" role="dialog"
+    <div className="modal d-block" id="demoDisclaimerModal" tabIndex="-1" role="dialog"
       aria-labelledby="demoDisclaimerModalTitle" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
-        <div className="modal-content text-primary">
-          <div className="modal-header d-inline">
+        <div className="modal-content text-white">
+          <div className="modal-header bg-danger d-inline">
             <h5 className="modal-title text-center" id="demoDisclaimerModalTitle">Welcome to Murican Dream</h5>
           </div>
-          <div className="modal-body">
-            This website is a content management application created for the purpose of <b>DEMOSTRATION ONLY</b>.
-            By choosing the accept button below you acknowledge that the merchandise shown here is <b>NOT</b> available for purchase,
-            that you will <b>NOT</b> provide genuine personal information,
-            and that you are aware no purchase will be processed.
+          <div className="modal-body bg-primary">
+            This website is a content management application created for the purpose of <b>DEMOSTRATION & ENTERTAINMENT ONLY</b>.
+            By choosing the accept button below you acknowledge:
+            <ol>
+              <li>The merchandise shown here is <b>NOT</b> available for purchase</li>
+              <li>You will <b>NOT</b> provide genuine personal information</li>
+              <li>You are aware no purchase will be processed</li>
+            </ol>
           </div>
-          <div className="modal-footer">
+          <div className="modal-footer bg-danger">
             <button type="button" className="btn btn-primary"
               onClick={() => props.acceptTerms()} data-dismiss="modal">Accept</button>
             <a href="https://jackmchou.com"><button type="button" className="btn btn-secondary"
