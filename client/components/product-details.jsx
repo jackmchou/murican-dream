905e3933ce11f51756d2b4e6531de5c952ff8f31
Jack Chou <jcacct@protonmail.ch>
2020-05-01 19:27:12 -0700

    Added withRouter from react router dom, changed props setparams to match params id to maintain state on refresh, styling edits for layout and color match

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import CartNaviModal from './modal';
+import { Link, withRouter } from 'react-router-dom';
+import CartNaviModal from './cart-navi-modal';
 
-export default class ProductDetails extends React.Component {
+class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -15,7 +15,7 @@ export default class ProductDetails extends React.Component {
   }
 
   componentDidMount() {
-    fetch(`/api/products/${this.props.setParams}`)
+    fetch(`/api/products/${this.props.match.params.id}`)
       .then(res => res.json())
       .then(product => this.setState({ product }))
       .catch(err => console.error(err))
@@ -34,12 +34,12 @@ export default class ProductDetails extends React.Component {
   render() {
     const { product } = this.state;
     return (
-      <div className="row no-gutters p-5">
+      <div className="row no-gutters mt-5 p-5">
         {
           this.state.isLoading ? <h1>Loading...</h1>
-            : <div className="card p-3">
+            : <div className="card text-white bg-dark p-3">
               <Link to="/">
-                <p className="text-dark w-25 m-2 interactable-element"> &lt; Back to Catalog</p>
+                <button className="m-2 btn btn-outline-info">Back to Catalog</button>
               </Link>
               <div className="row">
                 <div className="col-md-5">
@@ -47,7 +47,7 @@ export default class ProductDetails extends React.Component {
                 </div>
                 <div className="card-body col-md-7">
                   <h4 className="card-title">{product.name}</h4>
-                  <p className="card-subtitle text-muted font-weight-bolder">$ {(product.price * 0.01).toFixed(2)}</p>
+                  <p className="card-subtitle text-info font-weight-bolder">$ {(product.price * 0.01).toFixed(2)}</p>
                   <p className="card-text">{product.shortDescription}</p>
                   <button className="btn btn-primary" onClick={this.addItem}>Add to Cart</button>
                 </div>
@@ -60,3 +60,4 @@ export default class ProductDetails extends React.Component {
     );
   }
 }
+export default withRouter(ProductDetails);
