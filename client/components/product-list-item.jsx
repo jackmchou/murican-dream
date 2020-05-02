3a7d6bd7e176f1360b641a47bde0c24f31c478c0
Jack Chou <jcacct@protonmail.ch>
2020-05-02 06:19:45 -0700

    Edited styles for list item card

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -6,11 +6,11 @@ function ProductListItem(props) {
   return (
     <div className="col-md-4 mb-3">
       <Link to={`/productdetails/${product.productId}`} className="text-decoration-none">
-        <div className="text-dark card h-100 interactable-element" onClick={() => props.setParams(product.productId)}>
+        <div className="text-white bg-dark card p-3 h-100 interactable-element">
           <img src={product.image} className="card-img-top h-50" alt={product.name} />
           <div className="card-body">
             <h5 className="card-title">{product.name}</h5>
-            <p className="card-subtitle text-muted">$ {(product.price * 0.01).toFixed(2)}</p>
+            <p className="card-subtitle text-info">$ {(product.price * 0.01).toFixed(2)}</p>
             <p className="card-text">{product.shortDescription}</p>
           </div>
         </div>
@@ -18,5 +18,4 @@ function ProductListItem(props) {
     </div>
   );
 }
-
 export default withRouter(ProductListItem);
