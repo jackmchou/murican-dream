ed44358a2a5e90ca60aa7b8c3f21b973c26d7fb7
Jack Chou <jcacct@protonmail.ch>
2020-05-02 06:09:27 -0700

    Edited styling for component, added edit links to productlist, made link tags into buttons instead

--- client/components/cartsummary.jsx
+++ client/components/cartsummary.jsx
@@ -7,25 +7,27 @@ export default function CartSummary(props) {
   const itemsArr = props.cart;
   const emptyCart = itemsArr.length === 0;
   return (
-    <div className="p-5">
-      <Link to="/">
-        <p className="text-dark m-3 w-25 interactable-element"> &lt; Back to Catalog</p>
+    <div className="p-5 mt-5">
+      <Link to="/productlist">
+        <button className="m-2 btn btn-outline-info">Back to Catalog</button>
       </Link>
-      <h1>My Cart</h1>
-      {
-        emptyCart ? <h1>No items in cart</h1>
-          : itemsArr.map(item => {
-            return <CartSummaryItem
-              key={item.productId}
-              item={item}
-              deleteCartItem={props.deleteCartItem} />;
-          })
-      }
-      <div>
-        <h3 className="d-inline">Subtotal: ${(itemsArr.reduce((cur, acc) => cur + acc.price, 0) * 0.01).toFixed(2)}</h3>
-        <Link to="/checkout">
-          <button className="btn btn-primary float-right" disabled={emptyCart}>Check Out</button>
-        </Link>
+      <div className="bg-dark p-4">
+        <h1 className="text-white">My Cart</h1>
+        {
+          emptyCart ? <h1>No items in cart</h1>
+            : itemsArr.map(item => {
+              return <CartSummaryItem
+                key={item.productId}
+                item={item}
+                deleteCartItem={props.deleteCartItem} />;
+            })
+        }
+        <div>
+          <h3 className="text-white d-inline">Subtotal: ${(itemsArr.reduce((cur, acc) => cur + acc.price, 0) * 0.01).toFixed(2)}</h3>
+          <Link to="/checkout">
+            <button className="btn btn-primary float-right" disabled={emptyCart}>Check Out</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
