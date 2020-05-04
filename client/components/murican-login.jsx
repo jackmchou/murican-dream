7e470577885c33ee926e2ebc653a9d381f6951fb
Jack Chou <jcacct@protonmail.ch>
2020-05-04 14:52:39 -0700

    Added components for authenication and redirect for Murican Dream

---
+++ client/components/murican-login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useHistory, useLocation, Link } from 'react-router-dom';
+import auth from '../lib/auth';
+
+export default function MuricanLogin(props) {
+  const history = useHistory();
+  const location = useLocation();
+
+  const { from } = location.state || { from: { pathname: '/productlist' } };
+  const login = () => {
+    auth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div className="modal-footer bg-danger">
+      <p>You must log in to view Murican Dream</p>
+      <button className="btn btn-primary" onClick={login}>Log in</button>
+      <Link to="/">
+        <button type="button" className="btn btn-secondary"
+          data-dismiss="modal" aria-label="Close">Nah</button>
+      </Link>
+    </div>
+  );
+}
