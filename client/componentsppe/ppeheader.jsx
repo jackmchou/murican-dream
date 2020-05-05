bda0aba17716c0109630ddebffdf677ec67f14eb
Jack Chou <jcacct@protonmail.ch>
2020-05-05 07:29:14 -0700

    Edited styles and icons for header component

--- client/componentsppe/ppeheader.jsx
+++ client/componentsppe/ppeheader.jsx
@@ -5,14 +5,14 @@ export default function PPEHeader(props) {
   return (
     <div className="container-fluid p-3 px-5 bg-primary text-white fixed-top">
       <NavLink to="/ppeproductlist">
-        <span className="text-dark h4 interactable-element">PPE Agora</span>
+        <span className="text-dark text-center h4 interactable-element">PPE Agora</span>
       </NavLink>
       <NavLink to="/ppecartsummary">
         <span className="text-dark h4 float-right">
           {props.ppeCartItemCount > 1
             ? props.ppeCartItemCount + ' items '
             : props.ppeCartItemCount + ' item '}
-          <i className="fas fa-shopping-cart interactable-element" />
+          <i className="fas fa-shopping-bag interactable-element" />
         </span>
       </NavLink>
     </div>
