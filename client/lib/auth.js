9b9a1ebf2b227bca506ef53e937abfad44d3e693
Jack Chou <jcacct@protonmail.ch>
2020-05-05 16:38:46 -0700

    Edited authenticate method to send fetch req to verify passphrase

--- client/lib/auth.js
+++ client/lib/auth.js
@@ -1,8 +1,17 @@
 const auth = {
   isAuthenticated: false,
-  authenticate(cb) {
-    auth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
+  authenticate(cb, passphrase) {
+    const req = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(passphrase)
+    };
+    fetch('/api/auth', req)
+      .then(res => {
+        if (res.status === 403) return;
+        if (res.status === 200) auth.isAuthenticated = true;
+      });
+    setTimeout(cb, 100);
   },
   signout(cb) {
     auth.isAuthenticated = false;
