aff7944c7c9ca751ea91012f43f2215aff328369
Jack Chou <jcacct@protonmail.ch>
2020-05-06 10:42:17 -0700

    Edited styles and layout for header, added links toabout, service and contact

--- client/componentsppe/ppeheader.jsx
+++ client/componentsppe/ppeheader.jsx
@@ -3,18 +3,34 @@ import { NavLink } from 'react-router-dom';
 
 export default function PPEHeader(props) {
   return (
-    <div className="container-fluid p-3 px-5 bg-primary text-white fixed-top">
-      <NavLink to="/ppeproductlist">
-        <span className="text-dark text-center h4 interactable-element">PPE Agora</span>
-      </NavLink>
-      <NavLink to="/ppecartsummary">
-        <span className="text-dark h4 float-right">
-          {props.ppeCartItemCount > 1
-            ? props.ppeCartItemCount + ' items '
-            : props.ppeCartItemCount + ' item '}
-          <i className="fas fa-shopping-bag interactable-element" />
-        </span>
-      </NavLink>
+    <div className="container-fluid p-3 px-4 text-white">
+      <div className="row">
+        <div className="col-7">
+          <i className="fas fa-head-side-mask fa-2x text-dark interactable-text mr-2"></i>
+          <NavLink to="/ppeproductlist">
+            <span className="text-dark h3 interactable-text">PPE Agora</span>
+          </NavLink>
+        </div>
+        <div className="col-5 d-flex justify-content-between h5">
+          <NavLink to="/ppeabout">
+            <span className="text-dark interactable-text">About</span>
+          </NavLink>
+          <NavLink to="/ppeproducts">
+            <span className="text-dark interactable-text">Products</span>
+          </NavLink>
+          <NavLink to="/ppecontact">
+            <span className="text-dark interactable-text">Contact</span>
+          </NavLink>
+          <NavLink to="/ppecartsummary">
+            <span className="text-dark h5 float-right interactable-text">
+              {props.ppeCartItemCount > 1
+                ? props.ppeCartItemCount + ' items '
+                : props.ppeCartItemCount + ' item '}
+              <i className="fas fa-shopping-bag interactable-text" />
+            </span>
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
