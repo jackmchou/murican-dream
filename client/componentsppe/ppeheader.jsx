a1b7724a81a54b7dfb55fa203ddef0770a2eab92
Jack Chou <jcacct@protonmail.ch>
2020-05-06 19:01:19 -0700

    Edited header styles and spacing

--- client/componentsppe/ppeheader.jsx
+++ client/componentsppe/ppeheader.jsx
@@ -5,21 +5,21 @@ export default function PPEHeader(props) {
   return (
     <div className="container-fluid p-3 px-4 text-white">
       <div className="row">
-        <div className="col-7">
+        <div className="col-md-7 col-sm-12">
           <i className="fas fa-head-side-mask fa-2x text-dark interactable-text mr-2"></i>
           <NavLink to="/ppeproductlist">
             <span className="text-dark h3 interactable-text">PPE Agora</span>
           </NavLink>
         </div>
-        <div className="col-5 d-flex justify-content-between h5">
+        <div className="col-md-5 col-sm-12 pt-2 d-flex justify-content-end h5">
           <NavLink to="/ppeabout">
-            <span className="text-dark interactable-text">About</span>
+            <span className="text-dark pr-2 interactable-text">About</span>
           </NavLink>
           <NavLink to="/ppeproducts">
-            <span className="text-dark interactable-text">Products</span>
+            <span className="text-dark pr-2 interactable-text">Products</span>
           </NavLink>
           <NavLink to="/ppecontact">
-            <span className="text-dark interactable-text">Contact</span>
+            <span className="text-dark pr-2 interactable-text">Contact</span>
           </NavLink>
           <NavLink to="/ppecartsummary">
             <span className="text-dark h5 float-right interactable-text">
