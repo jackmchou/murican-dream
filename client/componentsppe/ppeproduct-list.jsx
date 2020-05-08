5b0848b6312f5b62ad0acfd6b79a628bdf25ea25
Jack Chou <jcacct@protonmail.ch>
2020-05-08 05:46:54 -0700

    Added nested containers for compact display on large screen

--- client/componentsppe/ppeproduct-list.jsx
+++ client/componentsppe/ppeproduct-list.jsx
@@ -27,30 +27,34 @@ class PPEProductList extends React.Component {
           <div className="display-3">Website Demo</div>
           <div className="h1 mb-5">Subtitles and text descriptions</div>
           <button className="mt-5 px-5 py-3 btn btn-warning"
-            onClick={() => scroll({ top: 800, left: 0, behavior: 'smooth' })}>Explore</button>
+            onClick={() => scroll({ top: 1000, left: 0, behavior: 'smooth' })}>Explore</button>
         </div>
-        <div className="row ppe-about pt-5 text-center">
-          <div className="col-md-4 col-sm-12"><i className="fas fa-pump-medical fa-10x"></i>
-            <p>Feature Text</p>
-          </div>
-          <div className="col-md-4 col-sm-12"><i className="fas fa-virus-slash fa-10x"></i>
-            <p>Feature Text</p>
+        <div className="container">
+          <div className="row ppe-about pt-5 mb-5 text-center">
+            <div className="col-md-4 col-sm-12"><i className="fas fa-pump-medical fa-10x"></i>
+              <p>Feature Text</p>
+            </div>
+            <div className="col-md-4 col-sm-12"><i className="fas fa-virus-slash fa-10x"></i>
+              <p>Feature Text</p>
 
-          </div>
-          <div className="col-md-4 col-sm-12"><i className="fas fa-shield-virus fa-10x"></i>
-            <p>Feature Text</p>
+            </div>
+            <div className="col-md-4 col-sm-12"><i className="fas fa-shield-virus fa-10x"></i>
+              <p>Feature Text</p>
+            </div>
           </div>
         </div>
-        <div className="row mt-5">
-          {
-            this.state.products.map(product => {
-              return (<PPEProductListItem
-                key={product.productId}
-                products={product}
-                setParams={this.props.setParams}
-              />);
-            })
-          }
+        <div className="container-lg">
+          <div className="row">
+            {
+              this.state.products.map(product => {
+                return (<PPEProductListItem
+                  key={product.productId}
+                  products={product}
+                  setParams={this.props.setParams}
+                />);
+              })
+            }
+          </div>
         </div>
       </div>
     );
