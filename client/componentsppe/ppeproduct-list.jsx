5722d7add5cf52d80edde2e3a6d516f7f4f11ba2
Jack Chou <jcacct@protonmail.ch>
2020-05-08 07:45:32 -0700

    Added story section

--- client/componentsppe/ppeproduct-list.jsx
+++ client/componentsppe/ppeproduct-list.jsx
@@ -43,19 +43,41 @@ class PPEProductList extends React.Component {
             }
           </div>
         </section>
-        <section className="container">
+        <section className="container-fluid overflow-hidden">
+          <div className="row justify-content-end d-none d-sm-flex">
+            <div className="col-10 bg-lightblue h-100-px"></div>
+          </div>
+          <div className="row">
+            <div className="col-sm-11 col-12 bg-dark py-5">
+              <div className="row py-sm-5">
+                <div className="col-lg-2 col-sm-1"></div>
+                <div className="col-lg-6 col-sm-8 col-12 text-white text-emphasis">
+                  <h1>Our Story</h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique expedita doloribus
+                    voluptatum architecto, dignissimos dolorem sint cumque atque nostrum quaerat sunt porro
+                    quae quis est, reprehenderit mollitia eaque minus veritatis!</p>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="row ppe-about pt-5 mb-5 text-center">
             <div className="col-md-4 col-sm-12"><i className="fas fa-pump-medical fa-10x pb-3"></i>
               <p>Feature Text</p>
-              <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit accusamus fugit consequatur eius alias veniam, ut temporibus explicabo nesciunt necessitatibus sunt quisquam! Obcaecati ex sequi quidem, perferendis unde consequuntur voluptatem!</small>
+              <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit accusamus fugit consequatur
+                eius alias veniam, ut temporibus explicabo nesciunt necessitatibus sunt quisquam! Obcaecati ex sequi
+                quidem, perferendis unde consequuntur voluptatem!</small>
             </div>
             <div className="col-md-4 col-sm-12"><i className="fas fa-virus-slash fa-10x pb-3"></i>
               <p>Feature Text</p>
-              <small>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde hic cupiditate aut recusandae laborum id quae saepe vitae illo soluta. Amet perferendis sunt eos impedit dolores illo praesentium tempore reprehenderit.</small>
+              <small>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde hic cupiditate aut recusandae
+                laborum id quae saepe vitae illo soluta. Amet perferendis sunt eos impedit dolores illo praesentium
+                tempore reprehenderit.</small>
             </div>
             <div className="col-md-4 col-sm-12"><i className="fas fa-shield-virus fa-10x pb-3"></i>
               <p>Feature Text</p>
-              <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt obcaecati laborum repellat alias. Ipsa, obcaecati. Ut molestias adipisci vero natus, quibusdam nihil consequatur, obcaecati ipsa repellendus non earum? Molestiae, veritatis.</small>
+              <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt obcaecati laborum repellat alias.
+                Ipsa, obcaecati. Ut molestias adipisci vero natus, quibusdam nihil consequatur, obcaecati ipsa repellendus
+                non earum? Molestiae, veritatis.</small>
             </div>
           </div>
         </section>
