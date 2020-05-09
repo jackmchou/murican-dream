8bd4614a106d047e57908c6e873f301778daf9ba
Jack Chou <jcacct@protonmail.ch>
2020-05-09 10:35:05 -0700

    Edited contact content, back to top button spacing

--- client/componentsppe/ppefooter.jsx
+++ client/componentsppe/ppefooter.jsx
@@ -4,11 +4,13 @@ export default function PPEFooter(props) {
   return (
     <div className="container-fluid footer bg-lightblue">
       <div className="row py-5">
-        <div className="col-7 pl-5 bg-primary">
+        <div className="col-7 pl-5 py-5 bg-primary">
           <h4>Contact US</h4>
+          <h5>TEL: 123-445-6443</h5>
+          <h5>E-mail: customers@ppeagora.com</h5>
         </div>
-        <div className="col-5 pr-5 border-left border-dark bg-primary">
-          <h4 className="mb-3 text-lg-center">Connect #PPEAGORA</h4>
+        <div className="col-5 pr-5 py-5 border-left border-dark bg-primary">
+          <h4 className="mb-3 text-center">Connect #PPEAGORA</h4>
           <div className="d-flex justify-content-around">
             <a className="badge badge-primary" href="https://instagram.com" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-instagram fa-3x"></i></a>
@@ -22,9 +24,9 @@ export default function PPEFooter(props) {
           </div>
         </div>
       </div>
-      <div className="row">
+      <div className="row py-2 bg-light">
         <div className="col-12 d-flex justify-content-center">
-          <button className="btn btn-light"
+          <button className="btn btn-light btn-lg font-weight-bold"
             onClick={() => scroll({ top: 0, left: 0, behavior: 'smooth' })}>BACK TO TOP ^</button>
         </div>
       </div>
