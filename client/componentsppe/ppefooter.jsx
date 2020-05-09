5bab9848672914c9341f9fddb3a7b99a3f9c0754
Jack Chou <jcacct@protonmail.ch>
2020-05-09 09:38:10 -0700

    Added initial footer content

--- client/componentsppe/ppefooter.jsx
+++ client/componentsppe/ppefooter.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 export default function PPEFooter(props) {
   return (
-    <div></div>
+    <div className="container-fluid footer bg-primary">
+      <div className="row p-5">
+        <div className="col-7">
+          <h3>Contact US</h3>
+        </div>
+        <div className="col-5">Connect</div>
+      </div>
+      <div className="row">
+        <div className="col-12 justify-content-center">
+          <button className="btn btn-light">BACK TO TOP ^</button>
+        </div>
+      </div>
+    </div>
   );
 }
