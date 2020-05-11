e42570b4a17be8881436b28573e2095eb55ee98b
Jack Chou <jcacct@protonmail.ch>
2020-05-11 16:14:50 -0700

    Added title component with appropriate props

--- client/componentsppe/ppelayout.jsx
+++ client/componentsppe/ppelayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PPEHeader from '../componentsppe/ppeheader';
 import PPEFooter from '../componentsppe/ppefooter';
+import Title from '../components/title';
 
 export default function PPELayOut(props) {
   return (
     <div className="ppe-bg overflow-auto">
+      <Title pageTitle="PPE Agora"/>
       <PPEHeader ppeCartItemCount={props.ppeCartItemCount} />
       {props.children}
       <PPEFooter />
