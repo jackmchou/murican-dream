acc021f0170a710089f062b6ed4f855cd845d596
Jack Chou <jcacct@protonmail.ch>
2020-06-16 10:35:13 -0700

    Changed implementation of login function, working on propert error feedback

--- client/components/murican-login.jsx
+++ client/components/murican-login.jsx
@@ -1,32 +1,37 @@
 import React, { useState } from 'react';
-import { useHistory, useLocation, Link } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import auth from '../lib/auth';
+import LinkButton from '../components/linkbutton';
 
 export default function MuricanLogin(props) {
   const history = useHistory();
   const location = useLocation();
   const [passphrase, setPass] = useState();
+  // const [errorFeedBack, setFeedBack] = useState();
   const { from } = location.state || { from: { pathname: '/productlist' } };
   const login = event => {
     event.preventDefault();
     auth.authenticate(() => {
       history.replace(from);
     }, { passphrase });
+    // if (!auth.isAuthenticated) {
+    //   setFeedBack('Incorrect passphrase')
+    // }
   };
-
+  const handleChange = event => {
+    setPass(event.target.value);
+  };
   return (
     <div className="modal-footer justify-content-center text-dark">
-      <form>
+      <form onSubmit={login}>
         <div className="form-group">
           <label htmlFor="passphrase">Passphrase</label>
-          <input onChange={event => setPass(event.target.value)} className="form-control" type="text" name="passphrase" />
+          {/* <small className="text-danger float-right">{errorFeedBack}</small> */}
+          <input onChange={handleChange} className="form-control" type="text" name="passphrase" title="Passphrase please" />
         </div>
         <p>You must enter the passphrase to view Murican Dream</p>
-        <button className="btn btn-primary mr-2" onClick={login}>Log in</button>
-        <Link to="/">
-          <button type="button" className="btn btn-secondary"
-            data-dismiss="modal" aria-label="Close">Nah</button>
-        </Link>
+        <button type="submit" className="btn btn-primary mr-2">Log in</button>
+        <LinkButton to="/" type="button" className="btn btn-secondary">Nah</LinkButton>
       </form>
     </div>
   );
